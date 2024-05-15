c60cb5549ecbb9acabb31d6d62a77be62fe7f241
Helio Frota <00hf11@gmail.com>
2024-05-15 16:43:02 -0300

    ipv6

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,19 @@
 use actix_web::{get, App, HttpResponse, HttpServer, Responder};
+use std::net::{IpAddr, Ipv6Addr, SocketAddr};
 
 #[get("/")]
 async fn hello() -> impl Responder {
     HttpResponse::Ok().body("Hello world!")
 }
 
+const DEFAULT_ADDR: SocketAddr = SocketAddr::new(IpAddr::V6(Ipv6Addr::LOCALHOST), 8080);
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     HttpServer::new(|| App::new().service(hello))
-        .bind(("127.0.0.1", 8080))?
+        .bind(DEFAULT_ADDR)?
+        // .bind("[::1]:8080")?
+        // .bind(("127.0.0.1", 8080))?
         .run()
         .await
 }
